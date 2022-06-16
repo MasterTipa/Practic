5430a0379f42ba6a6e73359026d8bd8d09cf3c71
MasterTipa <71173344+MasterTipa@users.noreply.github.com>
2022-06-16 16:53:32 +0300

    Add files via upload

---
+++ 3/CafeRegister/src/main/java/com/register/CafeRegister/CafeRegisterApplication.java
@@ -0,0 +1,27 @@
+package com.register.CafeRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CafeRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeRegisterApplication.class, args);
+	}
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ 3/CafeRegister/src/test/java/com/register/CafeRegister/CafeRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.register.CafeRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
